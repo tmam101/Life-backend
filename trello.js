b543566115f10879128c5d7edb10ced0a87a6054
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-03-11 00:57:47 -0500

    Update trello.js

--- trello.js
+++ trello.js
@@ -5,7 +5,7 @@ var baseURL = "https://api.trello.com/"
 var boardsURL = baseURL + "1/members/me/boards?key=" + key + "&token=" + token
 var network = require('network.js')
 
-function getBoards() {
+async function getBoards() {
   var response = await network.get(boardsURL)
   return response
 }
