784aa031238a4cce141c181079293a68b8ea0bff
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-03-11 01:00:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var trello = require('./trello.js');
 async function getBoards() {
-  print("started")
+  console.log("started")
   var response = await trello.getBoards()
   console.log(response)
   console.log("done")
