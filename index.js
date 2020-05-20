3d431b51b2564e6d5f0d6f969234c099286fc8f3
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-05-20 13:04:54 -0400

    Add requirements, get /

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+var envvar = require('envvar');
+var bodyParser = require('body-parser');
+var trelloKey = envvar.string('trellokey')
+var trelloSecret = envvar.string('trellotoken')
+var express = require('express');
+
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.get('/', function(request, response, next) {
+  console.log("get")
+})
 console.log("hey")
-// the issue is its not connected to the right github account! none of the changes I've been making have taken effect. 
+// the issue is its not connected to the right github account! none of the changes I've been making have taken effect.
---
+++ network.js
@@ -0,0 +1,43 @@
+const HTTPS     = require('https');
+const request = require('request')
+
+//MARK: HTTPS FUNCTIONS
+async function get(url) {
+  var promise = new Promise(function(resolve, reject) {
+    HTTPS.get(url, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        // console.log(data)
+        // Do the actual work
+        resolve(JSON.parse(data))
+      });
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+      reject()
+    });
+  });
+  return promise
+}
+
+async function post(url, json) {
+  return new Promise((resolve, reject) => {
+    console.log("post url: " + url)
+    request.post(url, json, (error, res, body) => {
+      if (error) {
+        console.log("POST url with error: " + url)
+        console.error(error)
+        reject(error)
+        return
+      }
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(body)
+      resolve({statusCode: res.statusCode, body: body})
+    })
+  })
+}
+
+exports.get = get;
+exports.post = post;
