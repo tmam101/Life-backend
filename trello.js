382987105dc2ac65bd3c20be42f4713a785dab3e
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-05-26 16:37:00 -0400

    Fix syntax error

--- trello.js
+++ trello.js
@@ -1,7 +1,7 @@
 var trelloKey = envvar.string('trellokey')
 var trelloSecret = envvar.string('trellotoken')
 var doingListID = "5e388f5d25b5df537d00585c"
-var doingCardsURL = "https://api.trello.com/1/lists/"+doingListID"/cards?fields=name&key="+trelloKey+"&token="+trellotoken
+var doingCardsURL = "https://api.trello.com/1/lists/"+doingListID+"/cards?fields=name&key="+trelloKey+"&token="+trellotoken
 
 async function getCardsFromDoing() {
   var response = await network.get(doingCardsURL)
