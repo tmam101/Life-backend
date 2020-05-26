30b3ff791cfd29c4c24fa0d2487b07b00f1c9a2f
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-05-26 16:40:35 -0400

    Change from secret to token

--- trello.js
+++ trello.js
@@ -1,8 +1,8 @@
 var envvar = require('envvar');
 var trelloKey = envvar.string('trellokey')
-var trelloSecret = envvar.string('trellotoken')
+var trelloToken = envvar.string('trellotoken')
 var doingListID = "5e388f5d25b5df537d00585c"
-var doingCardsURL = "https://api.trello.com/1/lists/"+doingListID+"/cards?fields=name&key="+trelloKey+"&token="+trellotoken
+var doingCardsURL = "https://api.trello.com/1/lists/"+doingListID+"/cards?fields=name&key="+trelloKey+"&token="+trelloToken
 
 async function getCardsFromDoing() {
   var response = await network.get(doingCardsURL)
