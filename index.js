2ddbb46737810e1c20e86cb27e2b99c9cd977f38
Thomas Goss <thomas.goss@willowtreeapps.com>
2020-05-28 11:13:55 -0400

    insert

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get('/db', async function(request, response) {
   app.get('/setup', async function(request, response) {
     // await query('DROP TABLE test_table')
     await query('CREATE TABLE IF NOT EXISTS stats (done int, todo int)')
-    // await query('INSERT INTO stats (0, 0)')
+    await query('INSERT INTO stats (0, 0)')
     response.json({done: await getDone()})
   }) // TODO update this every day
 
